9113a6577338c13d46f72488020f89f8a06fbd76
wolfene2001 <wolfene2001@yahoo.com>
2019-11-12 12:18:25 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,13 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(young_ones)
+  young_ones.collect do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
