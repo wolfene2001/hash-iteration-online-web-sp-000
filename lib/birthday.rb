d8c9e877064805840d756a731d31a0847a9b04ff
wolfene2001 <wolfene2001@yahoo.com>
2019-11-12 12:11:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,11 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(young_ones)
+  young_ones.collect do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
